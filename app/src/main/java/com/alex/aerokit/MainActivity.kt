6704efef36769436c667caa477171a552f1e2fae
Alexandre Ouimet <ouimet.alexandre94@gmail.com>
2025-07-09 14:18:18 -0400

    basic directory

--- app/src/main/java/com/alex/aerokit/MainActivity.kt
+++ app/src/main/java/com/alex/aerokit/MainActivity.kt
@@ -3,45 +3,22 @@ package com.alex.aerokit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.alex.aerokit.ui.theme.AeroKitTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.alex.aerokit.ui.AeroKitApp
 
+/**
+ * MainActivity launches the main composable for the app.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            AeroKitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface {
+                    AeroKitApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AeroKitTheme {
-        Greeting("Android")
     }
 }
